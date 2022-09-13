c7f8458cc393fb9ff7b0b71b3e76ef7d767e90d2
Thuong TC <truongquantruong68@gmail.com>
2022-09-12 19:21:45 -0700

    Commit inital code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import browser from "../../services/browser";
 export default async function handler(req, res) {
-  try {
-    let data = await (await browser()).getBlackListAccount();
-    res.status(200).json(data)
-  } catch (error) {
-    res.status(200).json(error)
-  }
+  let data = await (await browser()).getBlackListAccount();
+  res.status(200).json(data)
 }
